29a1316e49b0766f66cb9734d8184dc12fdd8fb0
carloscMe <carloscarrillo.sprout@gmail.com>
2025-03-21 17:08:35 -0500

    colores rgba

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/*const inputRojo = document.getElementById("rojo");
+const inputVerde = document.getElementById("verde");
+const inputAzul = document.getElementById("azul");
+const inputAlpha = document.getElementById("alpha");
+
+const textoRojo = document.getElementById("texto-rojo");
+const textoVerde = document.getElementById("texto-verde");
+const textoAzul = document.getElementById("texto-azul");
+const textoAlpha = document.getElementById("texto-alpha");
+
+let rojo = inputRojo.value;
+let verde = inputVerde.value;
+let azul = inputAzul.value;
+let alpha = inputAlpha.value;
+
+textoRojo.innerText = rojo;
+textoVerde.innerText = verde;
+textoAzul.innerText = azul;
+textoAlpha.innerText = alpha;
+
+
+function actualizarColor(rojo,verde,azul, alpha) {
+    const colorRGB = `rgb(${rojo}, ${verde}, ${azul}, ${alpha})`;
+    document.body.style.backgroundColor = colorRGB;
+}
+
+inputRojo.addEventListener("change", () =>{
+   rojo = inputRojo.value;
+  textoRojo.innerText = rojo;
+  actualizarColor(rojo, verde, azul, alpha)
+});
+
+inputVerde.addEventListener("change", () =>{
+    verde = inputVerde.value;
+   textoVerde.innerText = verde;
+   actualizarColor(rojo, verde, azul, alpha)
+ });
+
+ inputAzul.addEventListener("change", () =>{
+    azul = inputAzul.value;
+   textoAzul.innerText = azul;
+   actualizarColor(rojo, verde, azul, alpha)
+ });
+
+ inputAlpha.addEventListener("change", () => {
+    alpha = inputAlpha.value;
+    textoAlpha.innerText = alpha;
+    actualizarColor(rojo, verde, azul, alpha)
+ });
+*/
